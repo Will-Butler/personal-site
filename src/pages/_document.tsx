981ea3ec3142a2c53928f83c9ff15fc2282f26c2
Will-Butler <wgb6dwp@virginia.edu>
2025-04-16 11:56:54 -0400

    Add google analytics.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,21 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-D1P0DN4DPQ"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-D1P0DN4DPQ');
+            `,
+          }}
+        />
         <link rel="icon" href="WBDarkBG.png" />
       </Head>
       <body>
