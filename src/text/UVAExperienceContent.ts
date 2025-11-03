ad625b167a203d89606e46afc473c0d2580bf797
Will-Butler <wgb6dwp@virginia.edu>
2025-11-02 20:25:45 -0500

    Update uva band text.

--- src/text/UVAExperienceContent.ts
+++ src/text/UVAExperienceContent.ts
@@ -1,13 +1,14 @@
 import React from "react";
 
 const UVAExperienceContent = {
-    "RUGBY_BLURB": "Led the UVA varsity rugby team to playoffs",
-    "HOONS_BLURB": "Managed/performed for a band as lead singer and guitarist",
-    "P4P_BLURB": "Served as logistics chair for Pancakes for Parkinson's",
-    "BEER5K_BLURB": "Organized a 5K fundraiser for the iHope Charity Foundation",
-    "RANCH_BLURB": "Labored on a ranch in San Antonio for two summers",
-    "MAHJONG_BLURB": "Created and participated in a weekly Mahjong group",
-    "VISAS_BLURB": "Held a weekly language exchange with international students"
+  RUGBY_BLURB: "Led the UVA varsity rugby team to playoffs",
+  HOONS_BLURB:
+    "Managed/performed for a band as lead singer, guitarist, and songwriter",
+  P4P_BLURB: "Served as logistics chair for Pancakes for Parkinson's",
+  BEER5K_BLURB: "Organized a 5K fundraiser for the iHope Charity Foundation",
+  RANCH_BLURB: "Labored on a ranch in San Antonio for two summers",
+  MAHJONG_BLURB: "Created and participated in a weekly Mahjong group",
+  VISAS_BLURB: "Held a weekly language exchange with international students",
 };
 
 export default UVAExperienceContent;
